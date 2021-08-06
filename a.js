1b031865fc5bf481edd9bcfa7b4443d3d1115922
Dũng PHan <dungcnttk51mta@gmail.com>
2021-08-06 09:01:12 +0700

    a

---
+++ a.js
@@ -0,0 +1,61 @@
+
+
+function myFunction(item, content) {
+    if (item === "<br>") {
+        var mybr = document.createElement('br');
+        content.appendChild(mybr);
+    }
+    else {
+        var doc = new DOMParser().parseFromString(item, "text/xml");
+        var html = doc.firstChild;
+
+        let mau = item.substring(13, 19);
+
+        html.color = mau;
+        // //  console.log(html.color);
+        // content.appendChild(html);
+
+
+        // let newfont = document.createElement("font");
+        var mySubString = item.substring(
+            item.lastIndexOf("=") + 11,
+            item.lastIndexOf("/") - 1
+        );
+        let newFont = document.createElement("font");
+        newFont.innerHTML = mySubString;
+        newFont.color = mau;
+        // setTimeout(function () {
+        // }, 10000);
+        content.appendChild(newFont);
+        //  console.log(mySubString);
+
+
+
+    }
+
+
+}
+
+fetch('t.txt')
+    .then(response => response.text())
+    .then(data => {
+        // Do something with your data
+        var content = document.getElementById('content');
+        var t = data;
+        var lstXmlStrings = t.split('\r\n');
+        // console.log(data);
+        var tmp = lstXmlStrings[5];
+        for (var i = 0; i < 30; i++) {
+            console.log(i, tmp[i]);
+        }
+
+
+        console.log(lstXmlStrings.length);
+        // lstXmlStrings.forEach((item) => myFunction(item, content));
+        lstXmlStrings.forEach((item, i) => {
+            setTimeout(() => {
+                myFunction(item, content);
+            }, i * 5);
+        });
+
+    });
